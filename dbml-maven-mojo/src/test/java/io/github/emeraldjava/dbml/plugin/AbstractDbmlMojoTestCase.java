d0c6317f7268dfe44f5ab3e58c0e5136e67d5cdb
emeraldjava <emeraldjava@protonmail.com>
2024-09-25 05:27:57 +0100

    Fix test harness test cases by manually loading MavenProject.

--- dbml-maven-mojo/src/test/java/io/github/emeraldjava/dbml/plugin/AbstractDbmlMojoTestCase.java
+++ dbml-maven-mojo/src/test/java/io/github/emeraldjava/dbml/plugin/AbstractDbmlMojoTestCase.java
@@ -1,89 +1,39 @@
 package io.github.emeraldjava.dbml.plugin;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.artifact.repository.ArtifactRepositoryPolicy;
-import org.apache.maven.artifact.repository.MavenArtifactRepository;
 import org.apache.maven.execution.DefaultMavenExecutionRequest;
 import org.apache.maven.execution.MavenExecutionRequest;
-import org.apache.maven.execution.MavenSession;
-import org.apache.maven.plugin.Mojo;
-import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.ProjectBuilder;
+import org.apache.maven.project.ProjectBuildingException;
 import org.apache.maven.project.ProjectBuildingRequest;
-import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
+import org.eclipse.aether.DefaultRepositorySystemSession;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
+/**
+ * Base class for all dbml mojo tests.
+ */
 abstract class AbstractDbmlMojoTestCase extends AbstractMojoTestCase {
-    //    protected Path getUnitTestDir() {
-//        return Paths.get(getBasedir(), "src", "test", "resources", "unit");
-//    }
-
-    protected Mojo loadMojo(Path temporaryFolder, String projectRoot, String profile, String executionId) throws Exception {
-        FileUtils.copyDirectory(new File(projectRoot), temporaryFolder.toFile());
-        MavenProject project = null;//readMavenProject(temporaryFolder, profile);
-        MavenSession session = newMavenSession(project);
-        MojoExecution execution = newMojoExecution("generate");
-        MojoExecution executionWithId = copyWithExecutionId(executionId, execution);
-        return lookupConfiguredMojo(session, executionWithId);
-    }
-
-//    protected MavenProject readMavenProject(Path basedir, String profile) throws Exception {
-//        LocalRepository localRepo = new LocalRepository(basedir.resolve("local-repo").toFile());
-//        DefaultRepositorySystemSession session = MavenRepositorySystemUtils.newSession();
-//
-//        // add localRepo - framework doesn't do this on its own
-//        //ArtifactRepository localRepox = createLocalArtifactRepository();
-//        //session.setLocalRepositoryManager(localRepo);//.setLocalRepository(localRepo);
-////        session.setLocalRepositoryManager(
-////                new SimpleLocalRepositoryManagerFactory(new DefaultLocalPathComposer()).newInstance(session, localRepo)
-////        );
-//        MavenExecutionRequest request = new DefaultMavenExecutionRequest().setBaseDirectory(basedir.toFile());
-//        if (profile != null) {
-//            request.addActiveProfile(profile);
-//        }
-//        ProjectBuildingRequest configuration = request.getProjectBuildingRequest()
-//                .setRepositorySession(session)
-//                .setResolveDependencies(true);
-//        MavenProject project = lookup(ProjectBuilder.class)
-//                .build(basedir.resolve("pom.xml").toFile(), configuration)
-//                .getProject();
-//        assertNotNull(project);
-//        return project;
-//    }
-
-    protected static Path newTempFolder() throws IOException {
-        final Path tempDir = Files.createTempDirectory("test");
-        tempDir.toFile().deleteOnExit();
-        return tempDir;
-    }
-
-    private MojoExecution copyWithExecutionId(String executionId, MojoExecution execution) {
-        MojoExecution executionWithId  = new MojoExecution(execution.getMojoDescriptor(), executionId);
-        executionWithId.setConfiguration(execution.getConfiguration());
-        return executionWithId;
-    }
 
     /**
-     * Generate a local repository
-     * @return local repository object
-     *
-     * via https://stackoverflow.com/questions/42216442/maven-plugin-testing-harness-session-getlocalrepository-returns-null
+     * Loads the maven project config for the test case.
+     * see https://github.com/apache/maven-plugin-testing/blob/maven-plugin-testing-3.3.0/maven-plugin-testing-harness/src/test/java/org/apache/maven/plugin/testing/ParametersMojoTest.java
+     * @param basedir
+     * @return
+     * @throws ProjectBuildingException
+     * @throws Exception
      */
-//    private ArtifactRepository createLocalArtifactRepository() {
-//        MavenArtifactRepository local = new MavenArtifactRepository("local",
-//                localRepo.toURI().toString(),
-//                new DefaultRepositoryLayout(),
-//                new ArtifactRepositoryPolicy(true, ArtifactRepositoryPolicy.UPDATE_POLICY_ALWAYS, ArtifactRepositoryPolicy.CHECKSUM_POLICY_IGNORE),
-//                new ArtifactRepositoryPolicy(true, ArtifactRepositoryPolicy.UPDATE_POLICY_ALWAYS, ArtifactRepositoryPolicy.CHECKSUM_POLICY_IGNORE)
-//        );
-//        return local;
-//    }
+    protected MavenProject readMavenProject( File basedir )
+            throws ProjectBuildingException, Exception
+    {
+        File pom = new File( basedir, "pom.xml" );
+        MavenExecutionRequest request = new DefaultMavenExecutionRequest();
+        request.setBaseDirectory( basedir );
+        ProjectBuildingRequest configuration = request.getProjectBuildingRequest();
+        configuration.setRepositorySession( new DefaultRepositorySystemSession() );
+        MavenProject project = lookup( ProjectBuilder.class ).build( pom, configuration ).getProject();
+        assertNotNull( project );
+        return project;
+    }
 }
--- dbml-maven-mojo/src/test/java/io/github/emeraldjava/dbml/plugin/DbmlGeneratorMojoTest.java
+++ dbml-maven-mojo/src/test/java/io/github/emeraldjava/dbml/plugin/DbmlGeneratorMojoTest.java
@@ -1,12 +1,15 @@
 package io.github.emeraldjava.dbml.plugin;
 
-import org.junit.jupiter.api.Test;
+import org.apache.maven.project.MavenProject;
+import org.junit.Test;
 
 import java.io.File;
-import java.nio.file.Path;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * DbmlGeneratorMojoTest
+ *
+ * Need to use the junit 4 annotation since maven-test-harness is linked to it.
+ */
 public class DbmlGeneratorMojoTest extends AbstractDbmlMojoTestCase {
 
     @Override
@@ -14,49 +17,15 @@ public class DbmlGeneratorMojoTest extends AbstractDbmlMojoTestCase {
         super.setUp();
     }
 
+    /**
+     * https://stackoverflow.com/questions/70921236/how-to-make-mavenproject-injected-into-the-mojo-during-test-lookup
+     *
+     * @throws Exception
+     */
     @Test
-    public void test_A() throws Exception {
-        DbmlGeneratorMojo mojo = getGenerateMojo();
-//        mojo.inputSpec = null;
-  //      mojo.inputSpecRootDirectory = null;
-
-
+    public void test_generate() throws Exception {
+        MavenProject project = readMavenProject( new File( "src/test/resources/projects/default" ) );
+        DbmlGeneratorMojo mojo = (DbmlGeneratorMojo) lookupConfiguredMojo( project, "generate" );
         mojo.execute();
-
-//        generateMojo.analyser = mock(ConfigurationXmlFileOutOfDateResolver.class);
-//        generateMojo.productConfigRootFolder = new File(getBasedir(), "src/test/resources/test-projects/generate/src/main/resources/xml");
-//        generateMojo.transformer = mock(ConfigurationMergeTransformer.class);
-//
-//        when(generateMojo.analyser.resolveFilesNeedingUpdate(any(File.class), any(File.class))).thenReturn(getNoFilesNeedingUpdate());
-//
-//        generateMojo.execute();
-//
-//        verify(generateMojo.transformer, never()).transform(any(Collection.class), any(File.class), any(ConfigurationType.class));
-    }
-
-    //@Test
-    public void xx() throws Exception {
-        File testPom = new File(getBasedir(), "src/test/resources/projects/default/pom.xml" );
-        //aseertNotNull(testPom);
-        //https://stackoverflow.com/questions/44009232/nosuchelementexception-thrown-while-testing-maven-plugin/65523947#65523947
-        DbmlGeneratorMojo mojo = new DbmlGeneratorMojo();
-        mojo = (DbmlGeneratorMojo) super.configureMojo(mojo, extractPluginConfiguration("dbml-maven-plugin", testPom));
-        mojo.execute();
-    }
-
-//    private DbmlGeneratorMojo getGenerateMojo() throws Exception {
-////        File testPom = new File(getBasedir(), "src/test/resources/projects/default/pom.xml" );
-//
-//        final Path tempDir = newTempFolder();
-//        DbmlGeneratorMojo mojo = (DbmlGeneratorMojo) loadMojo(tempDir, "src/test/resources/projects/default", "file", "executionId");
-//        //DbmlGeneratorMojo mojo = (DbmlGeneratorMojo) s.lookupMojo( "generate", testPom );
-//        return mojo;
-//    }
-
-    private DbmlGeneratorMojo getGenerateMojo() throws Exception {
-        File testPom = new File(getBasedir(), "src/test/resources/projects/default/pom.xml" );
-        DbmlGeneratorMojo mojo = (DbmlGeneratorMojo) super.lookupMojo( "generate", testPom );
-        //super.look
-        return mojo;
     }
 }
