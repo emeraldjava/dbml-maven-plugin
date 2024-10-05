57bad45c9bfe1a69b69c2c5fa5958114570acd19
emeraldjava <emeraldjava@protonmail.com>
2024-10-05 08:03:44 +0100

    Add mustache/plantuml dependencies and some 101 code.

--- dbml-generator/src/main/java/io/github/emeraldjava/dbml/generator/DbmlGeneratorImpl.java
+++ dbml-generator/src/main/java/io/github/emeraldjava/dbml/generator/DbmlGeneratorImpl.java
@@ -1,9 +1,76 @@
 package io.github.emeraldjava.dbml.generator;
 
+import com.samskivert.mustache.Mustache;
+import com.wn.dbml.compiler.DbmlParser;
+import com.wn.dbml.model.Database;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import net.sourceforge.plantuml.FileFormat;
+import net.sourceforge.plantuml.FileFormatOption;
+import net.sourceforge.plantuml.SourceStringReader;
+
+import java.io.ByteArrayOutputStream;
+import java.io.StringWriter;
+import java.nio.charset.Charset;
+
+@Slf4j
 public class DbmlGeneratorImpl implements DbmlGenerator {
+
+    @Getter
+    @Setter
+    Mustache.Compiler compiler = Mustache.compiler();
 
     @Override
     public String generate() {
+
+        // get the DBML db
+        String dbml = """
+        Table table1 {
+          id integer
+        }""";
+        Database database = DbmlParser.parse(dbml);
+        log.info("{}",database.getSchema("public").getTables().toString());
+
+        // mustache
+        jmustache(database.getSchema("public").getTables());
+
+        // to plantuml svg
+
         return "dostuff";
+    }
+
+    /**
+     * https://github.com/samskivert/jmustache
+     * use the {{this}} trick
+     */
+    private void jmustache(Object input) {
+        String tmpl = "debug{{this}} {{#persons}}{{name}}: {{age}}\n{{/persons}}";
+        StringWriter writer = new StringWriter();
+        Mustache.compiler().compile(tmpl).execute(input,writer);
+        log.info("{}",writer.toString());
+    }
+
+    /**
+     * https://plantuml.com/api
+     */
+    private void plantUmlToSvg() throws Exception {
+        String source = "@startuml\n";
+        source += "Bob -> Alice : hello\n";
+        source += "@enduml\n";
+
+        SourceStringReader reader = new SourceStringReader(source);
+        final ByteArrayOutputStream os = new ByteArrayOutputStream();
+        // Write the first image to "os"
+        String desc = reader.generateImage(os, new FileFormatOption(FileFormat.SVG));
+        os.close();
+
+        // The XML is stored into svg
+        final String svg = new String(os.toByteArray(), Charset.forName("UTF-8"));
+    }
+
+    public static void main(String[] args) {
+        DbmlGeneratorImpl impl = new DbmlGeneratorImpl();
+        impl.generate();
     }
 }
